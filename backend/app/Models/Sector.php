16a59e08076d638fa723ecf7d4947af487bb4105
erbaum <erbaum@taltech.ee>
2026-08-10 12:46:45 +0300

    Update sector attribute

--- backend/app/Models/Sector.php
+++ backend/app/Models/Sector.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\WithoutIncrementing;
+use Illuminate\Database\Eloquent\Attributes\WithoutTimestamps;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+#[WithoutIncrementing]
+#[WithoutTimestamps]
 class Sector extends Model
 {
-    public $incrementing = false;
-    public $timestamps = false;
-
     public function parent(): BelongsTo
     {
         return $this->belongsTo(self::class, 'parent_id');
