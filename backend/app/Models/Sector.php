cb8a685cf2fe082751698bac8cb522735d72ce57
erbaum <erbaum@taltech.ee>
2026-08-10 11:44:44 +0300

    Create sector model

--- backend/app/Models/Sector.php
+++ backend/app/Models/Sector.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Sector extends Model
 {
-    //
+    public $incrementing = false;
+    public $timestamps = false;
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
 }
